a7c7967bcd65997acef2dfd5fd66988d3e43c673
nexussytem <nexussystemm@gmail.com>
2024-08-24 13:51:59 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -2,40 +2,179 @@
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bienvenido!</title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="icon" href="logo pestaña.icon">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <link rel="stylesheet" href="stile.css">
+    <link rel="icon" href="img/logo pestaña.icon">
+    <title>bienvenidos a NEXUS</title>
 </head>
 <body>
-    <div class="formulario">
-        <h1>Registrate</h1>
-        <form action="php/registro_usuario_be.php" method="post">
-            <div class="username">
-                <input type="text" placeholder="nombre y apellido" name="nombre_y_apellido">
-            
-            </div>
-            <div class="username">
-                <input type="text" placeholder="nombre de usuario" name="nombre_de_usuario">
-                
-            </div>
-            <div class="username">
-                <input type="text" placeholder="correo" name="correo">
-
-            </div>
-
-            <div class="username">
-                <input type="password" placeholder="contraseña" name="contraseña">
-                
-            </div>
-            <input type="submit" value="registrar">
-            <div class="registrarse">
-                Tienes cuenta? <a href="http://localhost/registro-login/indexlogin.php">inicia sesion!</a>
-            </div>
-        </form>
     
+  <div class="head">
+
+    <div class="logo">
+        <a href="#">NEXUS</a>
     </div>
 
-<body>
-<html>
+    <div class="navbar">
+        <a href="#inicio">inicio</a>
+        <a href="#nosotros">nosotros</a>
+        <a href="#entretenimiento">entretenimiento</a>
+
+    </div>
+
+  </div>
+
+    <header class="content header" id="inicio">
+        <h2 class="title">¡Bienvenido a NEXUS!</h2>
+        <p class="P">Nos complace tenerte aquí en nuestra comunidad dedicada a ofrecerte
+             diversas oportunidades para ganar dinero en Venezuela. En NEXUS,
+              creemos que todos merecen la posibilidad de mejorar su calidad de vida y
+               alcanzar sus metas financieras. 
+        </p>
+        <div class="btn-home">
+            <a href="http://localhost/registro-login/index.php" class="btn">¡Empieza ahora!</a>
+            <a href="https://www.youtube.com/@NEXUSOFICIAL-n8i" class="btn">saber mas</a>
+
+        </div>
+    
+    </header>
+    
+    <section class="content sau">
+        
+        <h2 class="title">Tipos de entradas</h2>
+        <P class="P"> Nuestro objetivo es brindarte las herramientas y el conocimiento necesarios
+             para que puedas maximizar tus ingresos de manera segura y efectiva.
+        </P>
+
+       <div class="box-container">
+       
+          <div class="box container ">
+            <i class="fab fa-black-tie"></i>
+            <h2>Afiliados</h2>
+            <p class="P">Si deseas empezar a generar dinero rapido y efectivo puedes elegir esta opción .</p>
+        
+          </div>
+          <div class="box container ">
+            <i class="fab fa-black-tie"></i>
+            <h2>Ventas de pantallas de streaming</h2>
+            <p class="P">Si te gusta el cine, trabajar haciendo lo que te hace feliz es la mejor opción
+                puedes revender las pantallas de streaming y generar dinero con ello.</p>
+        
+          </div>
+
+          <div class="box container ">
+            <i class="fab fa-black-tie"></i>
+            <h2>zona de peliculas y libros</h2>
+            <p class="P">En la zona de peliculas y libros encontraras entretenimiento gratuito
+                 brindado por la plataforma.
+            </p>
+        
+          </div>
+        </div>
+
+
+    </section>
+    
+    <section class="content about" id="nosotros">
+        <h2 class=title>NOSOTROS</h2>
+        <p class="P"> <em>Transparencia</em>: En Nexus, nos comprometemos a ser completamente transparentes sobre 
+            nuestras operaciones, términos de servicio y políticas de privacidad. Cualquiera 
+            puede acceder a esta información desde nuestro sitio web, lo que permite a los
+             usuarios comprender cómo funcionan nuestros servicios y cómo gestionamos sus datos.</p>
+
+        <p><em>Testimonios de usuarios</em>: A lo largo del tiempo, hemos recibido numerosas reseñas
+             y testimonios de usuarios satisfechos que han compartido sus experiencias positivas 
+             con Nexus. Estos relatos son una prueba de nuestro compromiso con la calidad 
+             y el servicio al cliente.
+        </p>
+
+        <p><em>Soporte al cliente</em>: Contamos con un equipo de atención al cliente accesible y capacitado
+             que está disponible para resolver cualquier duda o problema que puedan tener nuestros
+              usuarios, estamos aquí para ayudar.
+        </p>
+        
+        <p><em>Certificaciones y seguridad</em>: Nexus cumple con todas las normativas y regulaciones que rigen
+             el comercio en línea. Además, utilizamos tecnologías avanzadas de seguridad para proteger
+              la información de nuestros usuarios.
+        </p>
+
+        <p>En conclusión, en Nexus nos esforzamos por ofrecer un servicio confiable y de calidad.
+             Estamos aquí para brindar una experiencia positiva a todos nuestros usuarios y demostrar
+              que somos una plataforma legítima. Si tienes alguna pregunta o inquietud, no dudes en 
+              ponerte en contacto con nosotros. Tu confianza es nuestra prioridad.
+            </p>
+
+        <p><em>¡Gracias por formar parte de la comunidad de Nexus!</em></p>    
+        <a href="https://www.youtube.com/@NEXUSOFICIAL-n8i" class="btn">saber mas</a>
+
+
+    </section>
+
+    <section class="content about">
+        <h2 class=title>PUEDES INTERACTUAR CON NUESTRO CANAL DE YOUTUBE</h2>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/FmgllXJI03o?si=r4MTj52ohSanCHi-" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <p class="P"> Un video donde podras encontrar un poco mas de informacion!</p>
+        
+
+
+    </section>
+
+<section class="content price">
+    <article class="contain ">
+        <h2 class="title">¿que es NEXUS y porque no es una estafa?</h2>
+        <p class="P">En el mundo digital actual, es natural que existan preocupaciones sobre 
+            la legitimidad de diversas plataformas y servicios en línea. En este sentido,
+             queremos aclarar algunas dudas frecuentes acerca de Nexus, nuestro sitio web.
+        </p>
+        <p>Nexus es una plataforma diseñada con el objetivo de mejorar su calidad de vida y
+            alcanzar sus metas financieras, ofrecer servicios, compartir información,
+              proporcionar productos, etc. Desde su fundación, hemos trabajado
+               arduamente para crear un espacio seguro y confiable para nuestros usuarios.</p
+
+    </article>
+
+</section>
+
+<section class="entretenimiento" id="entretenimiento">
+    <h2 class=title>entretenimiento</h2>
+    
+    
+    <p class="P"> Bienvenido a nuestro rincón del entretenimiento, donde la diversión y
+         el conocimiento se combinan para brindarte experiencias únicas. Aquí encontrarás
+          una cuidadosa selección de libros, cursos y películas, pensados para inspirarte,
+           desafiar tu mente y, por supuesto, ofrecerte momentos de alegría y reflexión.
+   </p>
+
+
+    <p> <em>Libros:</em> Sumérgete en un universo literario con nuestras recomendaciones de libros.
+         Desde novelas cautivadoras hasta guías prácticas, aquí encontrarás lecturas que
+          despertarán tu imaginación y ampliarán tus horizontes. No importa cuál sea tu 
+          género favorito, tenemos algo para cada amante de las palabras.
+    </p>
+    
+    <p><em>Cursos</em>:¿Quieres aprender algo nuevo? Nuestros cursos están diseñados para 
+        satisfacer tu curiosidad. Ya sea que busques mejorar tus habilidades profesionales,
+         explorar un nuevo hobby o profundizar en un tema de tu interés, aquí hay recursos
+          para todos los niveles. Aprende a tu propio ritmo y descubre tu potencial.
+    </p>
+
+    <p><em>Películas:</em>  Relájate y disfruta de nuestra selección de películas, 
+        donde la magia del cine cobra vida. Desde clásicos atemporales hasta los
+         últimos estrenos, hemos compilado una lista que te hará reír, llorar y
+          reflexionar. Acompáñanos en este viaje visual y déjate llevar por las 
+          historias que nos conectan.
+        </p>
+
+    <p>En nuestro apartado de entretenimiento, estamos comprometidos a ofrecerte
+         contenido de calidad que enriquezca tu día a día. Explora, aprende y diviértete
+          en cada uno de nuestros subapartados. ¡Comienza tu aventura hoy mismo!</p>    
+          <a href="https://www.youtube.com/@NEXUSOFICIAL-n8i" class="btn">saber mas</a>
+</section>
+
+
+           
+
+
+</body> 
+</html>
